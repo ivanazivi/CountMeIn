d46a1594e7726d9dcf75ffbadee0148d62245d2e
divljiboy <ivan93.ns@hotmail.com>
2017-04-05 20:15:45 +0200

    Added facebook login

---
+++ CountMeIn/app/src/main/java/Fragments/MyFirstFragment.java
@@ -0,0 +1,21 @@
+package Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.countmein.countmein.R;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+/**
+ * Created by Home on 4/5/2017.
+ */
+
+public class MyFirstFragment extends Fragment {
+
+}
---
+++ CountMeIn/app/src/main/java/com/countmein/countmein/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.countmein.countmein;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        callbackManager = CallbackManager.Factory.create();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
