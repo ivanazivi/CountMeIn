e0e9538374d15f85663ca58b09a39faee96837bb
divljiboy <ivan93.ns@hotmail.com>
2017-04-19 21:15:11 +0200

    Cheanged color added toolbar

--- CountMeIn/app/src/main/java/com/countmein/countmein/HomeActivity.java
+++ CountMeIn/app/src/main/java/com/countmein/countmein/HomeActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -27,6 +29,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -73,17 +77,16 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    /*@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-        startActivity(intent);
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
 
-
-    }*/
 
     public List<Chat> getData(){
 
